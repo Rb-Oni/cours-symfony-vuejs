f68f41da8fb6066b7f7c5a351233b4779838c0c0
Robin FALCK <robin.falck57@gmail.com>
2022-08-09 10:27:38 +0200

    tailwindcss installation

--- webpack.config.js
+++ webpack.config.js
@@ -91,6 +91,8 @@ Encore
     
     // uncomment if you're having problems with a jQuery plugin
     .autoProvidejQuery()
+
+    .enablePostCssLoader()
 ;
 
 module.exports = Encore.getWebpackConfig();
