0f69a3b2b73eda07814fcade49fec4e8e150e318
Robin FALCK <robin.falck57@gmail.com>
2022-08-10 17:03:57 +0200

    showing/hiding content with v-show & v-if

--- assets/controllers/contact_controller.js
+++ assets/controllers/contact_controller.js
@@ -2,6 +2,7 @@ import { Controller } from '@hotwired/stimulus';
 import { createApp } from 'vue/dist/vue.esm-bundler';
 import Count from '../js/components/Count'
 import UserInput from '../js/components/UserInput'
+import ShowingContent from '../js/components/ShowingContent'
 import '../styles/app.css'
 
 export default class extends Controller {
@@ -13,12 +14,14 @@ export default class extends Controller {
             },
             components: {
                 Count,
-                UserInput
+                UserInput,
+                ShowingContent
             },
             template: `
                 <div class="max-w-screen-lg shadow-2xl rounded-lg mx-auto text-center py-12 mt-4">
                     <count></count>
                     <userInput></userInput>
+                    <showingContent></showingContent>
                 </div>
             `
         });
---
+++ assets/js/components/ShowingContent.js
@@ -0,0 +1,45 @@
+export default {
+    name: 'ShowingContent',
+    data() {
+        return {
+            color: '',
+            favoriteMessage: ''
+        };
+    },
+    methods: {
+        checkForm() {
+            if (!this.color) {
+                alert('Please enter a color');
+                return;
+            }
+            this.favoriteMessage = 'That is not my favorite color';
+
+            if (this.color == 'blue') {
+                this.favoriteMessage = 'That is my favorite, too !';
+            }
+
+            this.showMessage = this.color == 'blue';
+        }
+    },
+    components: {
+    },
+    template: `
+        <div class="bg-red-500 text-white py-6">
+            <h2 class="mb-4 text-3xl font-bold">
+            User Input
+            </h2>
+            <form @submit.prevent="checkForm()" class="text-black">
+                <div class="flex flex-col mb-4">
+                    <label for="">Your Favorite Color</label>
+                    <input type="text" v-model="color"/>
+                </div>
+                <div>
+                    <button class="bg-white hover:bg-gray-200 duration-150 font-bold py-2 px-6 shadow rounded-md">Submit</button>
+                </div>
+            </form>
+            <div v-if="favoriteMessage">
+                {{ favoriteMessage }}
+            </div>
+        </div>
+    `
+}
